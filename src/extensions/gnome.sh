12a4e0172fd9e3fc64d90cdf64d558b4a0278813
Ahmad Raza <pro.se.ahmad.raza.1@gmail.com>
2023-12-23 18:08:01 +0500

    revert back to `dconf` sol

--- src/extensions/gnome.sh
+++ src/extensions/gnome.sh
@@ -1,31 +1,33 @@
 #!/bin/zsh
 
+TMP=/tmp
 CWD=$PWD
-ensure_dependencies git
+ensure_dependencies dconf git
 
 orchis_gtk() {
   local NAME="Orchis-theme"
 
+  cd $TMP;
   redirect_output git clone -q https://github.com/vinceliuice/$NAME
   cd $NAME || exit
   redirect_output ./install.sh -l -t green -s compact --tweaks compact
   cd $CWD
-  redirect_output rm -rf $NAME
 }
 
 tela_icons() {
   local NAME="Tela-icon-theme"
 
+  cd $TMP;
   redirect_output git clone -q https://github.com/vinceliuice/${NAME}.git
   cd $NAME || exit
   redirect_output zsh ./install.sh -n manjaro
   cd $CWD
-  redirect_output rm -rf $NAME
 }
 
 vimix_cursors() {
   local NAME="Vimix-cursors"
 
+  cd $TMP;
   redirect_output git clone -q https://github.com/vinceliuice/$NAME
   cd $NAME || exit
   redirect_output ./install.sh
@@ -33,54 +35,6 @@ vimix_cursors() {
   redirect_output cp -rfp $HOME/.local/share/icons/Vimix* $HOME/.icons/ # Should be installed to ~/.icons
   redirect_output rm -rf $HOME/.local/share/icons/Vimix*
   cd $CWD
-  redirect_output rm -rf $NAME
-}
-
-configure_interface() {
-  # Function to replace or add an entry in a file
-  replace_or_add_entry() {
-    file=$1
-    entry=$2
-    new_line=$3
-
-    if grep -q "^$entry" "$file"; then
-      sed -i "s|^$entry.*|$new_line|g" "$file"
-    else
-      echo "$new_line" >> "$file"
-    fi
-  }
-
-  CURSOR_THEME=$1
-  ICON_THEME=$2
-  GTK_THEME=$3
-
-  redirect_output mkdir -p $HOME/.config/gtk-3.0
-  redirect_output mkdir -p $HOME/.config/gtk-4.0
-  touch $HOME/.Xresources $HOME/.gtkrc-2.0 $HOME/.config/gtk-3.0/settings.ini $HOME/.config/gtk-4.0/settings.ini
- 
-  incremental_backup $HOME/.Xresources
-  incremental_backup $HOME/.config/gtk-3.0/settings.ini
-  incremental_backup $HOME/.config/gtk-4.0/settings.ini
-
-  # Add Settings section to GTK3+ files
-  replace_or_add_entry $HOME/.config/gtk-3.0/settings.ini "[Settings]" "[Settings]"
-  replace_or_add_entry $HOME/.config/gtk-4.0/settings.ini "[Settings]" "[Settings]"
-
-  # Set Cursor Theme
-  print_feedback_str 3 "Setting Cursor Theme: ${CURSOR_THEME}"
-  replace_or_add_entry $HOME/.Xresources "Xcursor.theme:" "Xcursor.theme: $CURSOR_THEME"
-
-  # Set Icon Theme
-  print_feedback_str 3 "Setting Icon Theme: ${ICON_THEME}"
-  replace_or_add_entry $HOME/.gtkrc-2.0 "gtk-icon-theme-name=" "gtk-icon-theme-name=\"$ICON_THEME\""
-  replace_or_add_entry $HOME/.config/gtk-3.0/settings.ini "gtk-icon-theme-name=" "gtk-icon-theme-name=\"$ICON_THEME\""
-  replace_or_add_entry $HOME/.config/gtk-4.0/settings.ini "gtk-icon-theme-name=" "gtk-icon-theme-name=\"$ICON_THEME\""
-
-  # Set GTK Theme
-  print_feedback_str 3 "Setting GTK Theme: ${GTK_THEME}"
-  replace_or_add_entry $HOME/.gtkrc-2.0 "gtk-theme-name=" "gtk-theme-name=\"$GTK_THEME\""
-  replace_or_add_entry $HOME/.config/gtk-3.0/settings.ini "gtk-theme-name=" "gtk-theme-name=$GTK_THEME"
-  replace_or_add_entry $HOME/.config/gtk-4.0/settings.ini "gtk-theme-name=" "gtk-theme-name=$GTK_THEME"
 }
 
 configure_workspaces() {
@@ -99,6 +53,25 @@ configure_workspaces() {
   done
 }
 
+configure_interface() {
+  CURSOR_THEME=$1
+  ICON_THEME=$2
+  GTK_THEME=$3
+  SHELL_THEME=$4
+
+
+  print_feedback_str 3 "Setting Cursor Theme: ${CURSOR_THEME}"
+  dconf write /org/gnome/desktop/interface/cursor-theme $CURSOR_THEME
+
+  print_feedback_str 3 "Setting Icon Theme: ${ICON_THEME}"
+  dconf write /org/gnome/desktop/interface/icon-theme $ICON_THEME
+
+  print_feedback_str 3 "Setting GTK Theme: ${GTK_THEME}"
+  dconf write /org/gnome/desktop/interface/gtk-theme $GTK_THEME
+
+  # TODO: Enable gnome shell theme
+}
+
 print_feedback_str 2 "Installing Orchis GTK Theme..."
 orchis_gtk
 
@@ -108,14 +81,8 @@ tela_icons
 print_feedback_str 2 "Installing Vimix Cursor Theme..."
 vimix_cursors
 
-print_feedback_str 2 "Configuring interface:"
-configure_interface "Vimix-white-cursors" "manjaro-dark" "Orchis-Green-Dark-Compact"
+print_feedback_str 2 "Configuring workspaces:"
+configure_workspaces 8
 
-ensure_dependencies -o dconf
-# Proceed with the task only if dependencies are met
-if [ $? -eq 0 ]; then
-  print_feedback_str 2 "Configuring workspaces:"
-  configure_workspaces 8
-else 
-  print_feedback_str 2 'Skipped Configuring Workspaces: `dconf` not found!'
-fi
+print_feedback_str 2 "Configuring interface:"
+configure_interface "'Vimix-white-cursors'" "'manjaro-dark'" "'Orchis-Green-Dark-Compact'"
